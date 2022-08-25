c016ec34c28c43adaa1d7f121eb4114f06322348
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-08-25 22:50:55 +0300

    create template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/custom.scss';
 
 const app = () => {
     console.log('start');
--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,36 @@ module.exports = {
     devServer: {
         port: 3000,
         host: '0.0.0.0',
+        liveReload: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use:['style-loader', 'css-loader'],
-            }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions:{
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader',
+                }]
+            },
         ]
     },
+
 };
